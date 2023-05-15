0840b359b5d6e1be96d6717e0866b47c2e899bc9
Jaydatta Dalvi <122354863+jaydalvi11@users.noreply.github.com>
2023-05-15 22:47:19 +0530

    Update login.java

--- login.java
+++ login.java
@@ -1,5 +1,7 @@
 public class hello{
     public static void main(String[] args) {
         System.out.println("Lol i forgot to create new branch.");
+        /*I want to create a merge conflict situation.
+        So I am doing this.*/
     }
 }
